4ff981dff8f5920501df142be53b383ceb5e032c
Thomas Schersach <tschersach@gmail.com>
2021-01-12 10:08:34 +0100

    Bump deps and update kube exemple

--- kubernetes/index.ts
+++ kubernetes/index.ts
@@ -1,20 +1,23 @@
 import * as k8s from '@pulumi/kubernetes'
 import * as eks from '@pulumi/eks'
 import * as awsx from "@pulumi/awsx";
-import * as aws from "@pulumi/aws";
-import * as pul from '@pulumi/pulumi'
 
 const appName = 'nginx'
 const appLabels = {app: appName}
 
 // Create a VPC for our cluster.
-const vpc = new awsx.ec2.Vpc('eks-vpc', {});
+const vpc = new awsx.ec2.Vpc("vpc", {subnets: [{type: "public"}]});
 
 // Create the EKS cluster, including a "gp2"-backed StorageClass and a deployment of the Kubernetes dashboard.
-const cluster = new eks.Cluster('eks-cluster', {
+// Create the EKS cluster itself and a deployment of the Kubernetes dashboard.
+const cluster = new eks.Cluster("cluster", {
     vpcId: vpc.id,
-    subnetIds: pul.all([vpc.publicSubnetIds, vpc.privateSubnetIds]).apply(([ids]) => ids)
-})
+    subnetIds: vpc.publicSubnetIds,
+    instanceType: "t2.medium",
+    desiredCapacity: 2,
+    minSize: 1,
+    maxSize: 2,
+});
 
 const deployment = new k8s.apps.v1.Deployment(`${appName}-dep`, {
     metadata: {labels: appLabels},
@@ -50,10 +53,3 @@ export const url = service.status.loadBalancer.ingress[0].hostname;
 
 // Export the cluster's kubeconfig.
 export const kubeconfig = cluster.kubeconfig;
-
-
-const group = new awsx.ec2.SecurityGroup("webserver-secgrp", {
-    ingress: [
-        {protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"]},
-    ],
-});
